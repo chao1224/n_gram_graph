7ae7bbbc1f322120ab27e108d3fb9a70b732cff5
chao1224 <shengchao.hacker@gmail.com>
2020-01-08 21:16:49 -0500

    Clean up dataset (2)

--- datasets/download_data.sh
+++ datasets/download_data.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/delaney-processed.csv
-wget -O malaria-processed.csv https://raw.githubusercontent.com/HIPS/neural-fingerprint/master/data/2015-06-03-malaria/malaria-processed.csv
-wget -O cep-processed.csv https://raw.githubusercontent.com/HIPS/neural-fingerprint/master/data/2015-06-02-cep-pce/cep-processed.csv
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/muv.csv.gz
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/tox21.csv.gz
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/clintox.csv.gz
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/HIV.csv
 mv HIV.csv hiv.csv
 
+wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/delaney-processed.csv
+wget -O malaria-processed.csv https://raw.githubusercontent.com/HIPS/neural-fingerprint/master/data/2015-06-03-malaria/malaria-processed.csv
+wget -O cep-processed.csv https://raw.githubusercontent.com/HIPS/neural-fingerprint/master/data/2015-06-02-cep-pce/cep-processed.csv
+
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/qm7.csv
-wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/gdb7.tar.gz
-tar -xzvf gdb7.tar.gz
-mv gdb7.sdf qm7.sdf
-rm gdb7.tar.gz
 
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/qm8.csv
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/gdb8.tar.gz
